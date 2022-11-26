44f7cafd081f599a2e709e2db82a61b35df8dc45
Miruna Chirita <chiritamiruna51@yahoo.ro>
2022-11-26 11:32:49 +0200

    Add project files.

---
+++ Chirita_Miruna_Lab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Chirita_Miruna_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chirita_Miruna_Lab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Chirita_Miruna_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
